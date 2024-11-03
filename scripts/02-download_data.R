d3d5efdb4b954a0d7850ccbf348be93571efc41a
Wendy Huang <yanfei.huang@mail.utoronto.ca>
2024-11-02 22:29:16 -0400

    commit

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,26 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from fivethirtyeight website (https://projects.fivethirtyeight.com/polls/president-general/2024/national/)
+# Author: Yanfei Huang
+# Date: 02 November 2024
+# Contact: yanfei.huang@mail.utoronto.ca
+# License: University of Toronto
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+#download the data from the website
+raw_data <- read.csv(
+  file = "https://projects.fivethirtyeight.com/polls/data/president_polls.csv")
 
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# name the raw data into raw_president_polls
+write_csv(raw_president_polls, "data/01-raw_data/raw_data.csv") 
 
          
